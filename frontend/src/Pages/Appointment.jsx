6c8f8f0b55e6a24cf8b5f4ccd97c477403cc7670
Saksham Lamba <sakshamlambamonu@gmail.com>
2024-12-10 22:28:22 +0530

    update: Added Time Slots for Appointment Booking

--- frontend/src/Pages/Appointment.jsx
+++ frontend/src/Pages/Appointment.jsx
@@ -22,51 +22,48 @@ const Appointment = () => {
     const getAvailableSlots = async () => {
         setDocSlots([]);
 
-        //Getting Current Date
         let today = new Date();
 
         for (let i = 0; i < 7; i++) {
-            //Getting Date with index
             let currentDate = new Date(today);
             currentDate.setDate(today.getDate() + i);
 
-            //Setting End Time of the Date with index
             let endTime = new Date();
             endTime.setDate(today.getDate() + i);
             endTime.setHours(21, 0, 0, 0);
 
-            //Setting Hours
-            if (today.getDate() === currentDate.getDate()) {
+            if (i === 0 && today.getHours() >= 21) {
+                continue;
+            }
+
+            if (i === 0) {
                 currentDate.setHours(
-                    currentDate.getHours() > 10
-                        ? currentDate.getHours() + 1
-                        : 10
+                    today.getHours() > 10 ? today.getHours() + 1 : 10
                 );
-                currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+                currentDate.setMinutes(today.getMinutes() > 30 ? 30 : 0);
             } else {
                 currentDate.setHours(10);
                 currentDate.setMinutes(0);
             }
 
             let timeSlots = [];
-
             while (currentDate < endTime) {
                 let formattedTime = currentDate.toLocaleTimeString([], {
                     hour: "2-digit",
                     minute: "2-digit",
                 });
 
-                //Add slot to array
                 timeSlots.push({
                     datetime: new Date(currentDate),
                     time: formattedTime,
                 });
 
-                //Increment current time by 30 minutes
                 currentDate.setMinutes(currentDate.getMinutes() + 30);
             }
 
-            setDocSlots((prev) => [...prev, timeSlots]);
+            if (timeSlots.length) {
+                setDocSlots((prev) => [...prev, timeSlots]);
+            }
         }
     };
 
@@ -91,7 +88,6 @@ const Appointment = () => {
                         <img className="docimg" src={docInfo.image} alt="" />
                     </div>
                     <div className="docinfo">
-                        {/* Doctor Info */}
                         <p className="docname">
                             {docInfo.name}{" "}
                             <img
@@ -108,7 +104,6 @@ const Appointment = () => {
                                 {docInfo.experience}
                             </button>
                         </div>
-                        {/* Doctor About */}
                         <div>
                             <p className="docabout">
                                 About <img src={assets.info_icon} alt="" />
@@ -120,7 +115,7 @@ const Appointment = () => {
                             <span className="docprice">
                                 {currencySymbol}
                                 {docInfo.fees}
-                            </span>{" "}
+                            </span>
                         </p>
                     </div>
                 </div>
@@ -131,7 +126,15 @@ const Appointment = () => {
                     <div className="slotdatetime">
                         {docSlots.length &&
                             docSlots.map((item, index) => (
-                                <div key={index}>
+                                <div
+                                    onClick={() => setSlotIndex(index)}
+                                    className={`slot-item ${
+                                        slotIndex === index
+                                            ? "selected-slot"
+                                            : "default-slot"
+                                    }`}
+                                    key={index}
+                                >
                                     <p>
                                         {item[0] &&
                                             daysOfWeek[
@@ -142,6 +145,22 @@ const Appointment = () => {
                                         {item[0] && item[0].datetime.getDate()}
                                     </p>
                                 </div>
+                            ))}
+                    </div>
+                    <div className="slottime">
+                        {docSlots.length &&
+                            docSlots[slotIndex].map((item, index) => (
+                                <p
+                                    onClick={() => setSlotTime(item.time)}
+                                    className={`time-slot ${
+                                        item.time === slotTime
+                                            ? "selected-time"
+                                            : "default-time"
+                                    }`}
+                                    key={index}
+                                >
+                                    {item.time.toLowerCase()}
+                                </p>
                             ))}
                     </div>
                 </div>
